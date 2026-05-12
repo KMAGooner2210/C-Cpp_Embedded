c849a162173bfbd60dbc9df27530fcf4890a09a4
KMAGooner2210 <thanhtunga7k44nknd@gmail.com>
2026-05-12 16:06:58 +0700

    Update Untitled2.c

--- Bai06_Bit_Manipulation/Ex10/Untitled2.c
+++ Bai06_Bit_Manipulation/Ex10/Untitled2.c
@@ -1,3 +1,15 @@
+// ## **Ex10.	Tìm Bit 1 Thấp Nhất**
+// o	Yêu cầu: Viết hàm tìm vị trí của bit 1 thấp nhất (tính từ 0) trong biểu diễn nhị phân của một số nguyên không dấu. Nếu không có bit 1, trả về -1.
+// ```
+// o	Ví dụ: 
+// 	Input: 5 (nhị phân: 00000101)
+// 	Output: 0 (bit 1 thấp nhất ở vị trí 0)
+// 	Input: 8 (nhị phân: 00001000)
+// 	Output: 3
+// 	Input: 0 (nhị phân: 00000000)
+// 	Output: -1
+// o	Gợi ý: Sử dụng value & (-value) để lấy bit thấp nhất, sau đó tìm vị trí bằng cách lặp hoặc mẹo logarit.
+// ```
 #include<stdio.h>
 #include<conio.h>
 #include<stdint.h>
